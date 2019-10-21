598cb341b5b76257c4798b448b96c8aa7f222974
Gabriel Dantas <gabriel.dantas@gethelix.org>
2019-10-21 08:48:37 -0300

    created install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo "Cloning project color-ls"
+git clone https://github.com/gabriel-dantas98/klyntar
+
+cd klyntar
+
+echo "Moving binary to /usr/bin/"
+sudo mv bin/color-ls /usr/bin/
+
+echo "Nice, you should start using color-ls :D"
+colorls help
